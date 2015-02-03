3f73a16ee91ccf1f9a44ab539eee6215b12d7925
fouber <fouber.NET@gmail.com>
2015-02-04 01:25:37 +0800

    [UPD] update some files.

--- .build.sh
+++ .build.sh
@@ -2,15 +2,15 @@
 
 if [ -d output ]; then
     echo "➥ Commit files"
-    git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
     cp -fr output/* ${REPO_SLUG}
     cd ${REPO_SLUG}
-    ls -l
+    ls -la
     git config user.email "travis@travis-ci.org"
     git config user.name "travis-ci"
     git add -A
     git commit -m "[ci skip] publish website"
-    git push -fq origin master > /dev/null
+    git push -fq origin gh-pages > /dev/null
 else
     echo '➥ Fail'
     exit 1
