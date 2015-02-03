20d412d76158cedd500c607aabbeefa47f229e6e
fouber <fouber.NET@gmail.com>
2015-02-04 01:20:22 +0800

    [UPD] update some files.

---
+++ .build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -d output ]; then
+    echo "➥ Commit files"
+    git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
+    cp -fr output/* ${REPO_SLUG}
+    cd ${REPO_SLUG}
+    ls -l
+    git config user.email "travis@travis-ci.org"
+    git config user.name "travis-ci"
+    git add -A
+    git commit -m "[ci skip] publish website"
+    git push -fq origin master > /dev/null
+else
+    echo '➥ Fail'
+    exit 1
+fi
