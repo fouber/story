cd6750ee4a68cf013ddf2d4003a246e9105090ae
fouber <fouber.net@gmail.com>
2016-01-08 00:25:37 +0800

    update

--- step1.bat
+++ step1.bat
@@ -1,6 +1,3 @@
-git add -A
-git commit -am "update"
-git push origin master
 set output=%TEMP%\story-output
 fis release -Doumpd %output%
 pause
---
+++ step3.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -am "update"
+git push origin master
